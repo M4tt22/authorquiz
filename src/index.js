d0c5e0515feb20fe72be0e12e325109701760b91
unknown <matthieu.pomies40@gmail.com>
2019-03-14 22:36:14 +0100

    add class
     Please enter the commit message for your changes. Lines starting
    add class

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+class Sum extends React.Component {
+    render() {
+        return <h1>
+        {this.props.a} + {this.props.b} = {this.props.a + this.props.b}
+        </h1>;
+    }
+}
+
+ReactDOM.render(<Sum a={4} b={2} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// function Sum(props)
+// {
+//     return <h1>
+//         {props.a} + {props.b} = {props.a + props.b}
+//     </h1>
+// };
