74f5dc17670ad7fa8dc02b00382538c5af3d8b05
unknown <matthieu.pomies40@gmail.com>
2019-04-02 23:03:29 +0200

    validate props

--- src/index.js
+++ src/index.js
@@ -3,16 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
 
+/*
 class Sum extends React.Component {
+    
+
     render() {
         return <h1>
         {this.props.a} + {this.props.b} = {this.props.a + this.props.b}
         </h1>;
     }
+
+    propTypes = {
+        a: PropTypes.number.isRequired,
+        b: PropTypes.number.isRequired,   
+    };
+}
+*/
+
+function Sum(props) {
+    return  <h1>
+        {props.a} + {props.b} = {props.a + props.b}
+    </h1>;
 }
 
-ReactDOM.render(<Sum a={4} b={2} />, document.getElementById('root'));
+Sum.propTypes = {
+    a: PropTypes.number.isRequired,
+    b: PropTypes.number.isRequired,   
+};
+
+ReactDOM.render(<Sum a={"a"} b={2} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
